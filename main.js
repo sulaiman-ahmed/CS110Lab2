0f1287613110839df189ea9af142f03f3be5fe11
sulaiman-ahmed <sulaimanahmed08@gmail.com>
2022-04-14 15:50:45 -0700

    finished timer

--- main.js
+++ main.js
@@ -5,19 +5,43 @@ var player1score = 0;
 var player2score = 0;
 var player1Array = [];
 var player2Array = [];
+var time = 5;
 document.getElementById("score1").innerHTML = player1score;
 document.getElementById("score2").innerHTML = player2score;
 
-startGame();
+document.getElementById("two_player").onclick = function() {startGame()};
 function startGame() {
+    gameOver = false;
     turn = true;
     turnCount = 0;
     player1score = 0;
     player2score = 0;
     player1Array = [];
     player2Array = [];
+    time = 5;
     document.getElementById("display").innerHTML = "It's your turn, Player 1.";
+    document.getElementById("timer").innerHTML = 'Time remaining: <span id="timeRemaining"></span> seconds';
+    document.getElementById("timeRemaining").innerHTML = time;
+    var x = setInterval(function() {
+        document.getElementById("timeRemaining").innerHTML = time--;
+        if(time < 0) {
+            time = 5;
+            document.getElementById("timeout_display").innerHTML = "Timeout! Next players turn.";
+            if(turn) {
+                turn = false;
+                document.getElementById("display").innerHTML = "It's your turn, Player 2.";
+            } else {
+                turn = true;
+                document.getElementById("display").innerHTML = "It's your turn, Player 1.";
+
+            }
+        }
+    }, 1000);
+
 }
+
+
+
 
 document.getElementById("one").onclick = function() {placeElement("one")};
 document.getElementById("two").onclick = function() {placeElement("two")};
@@ -33,18 +57,21 @@ document.getElementById("new_game").onclick = function() {newGame()};
 document.getElementById("reset").onclick = function() {resetGame()};
 
 function placeElement(elem) {
+    document.getElementById("timeout_display").innerHTML = "";
     if(turn) {
         document.getElementById(elem).innerHTML = '<span class="xo"> X</span>';
         player1Array.push(elem);
         turn = false;
         turnCount++;
         document.getElementById("display").innerHTML = "It's your turn, Player 2.";
+        time = 5;
     } else {
         document.getElementById(elem).innerHTML = '<span class="xo"> O</span>';
         player2Array.push(elem);
         turn = true;
         turnCount++;
         document.getElementById("display").innerHTML = "It's your turn, Player 1.";
+        time = 5;
     }
 
     if((player1Array.includes("one") && player1Array.includes("two") && player1Array.includes("three"))
